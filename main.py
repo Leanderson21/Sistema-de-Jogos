4551d972c18e12ffc5ea239de5832c80f6496b8d
LeandersonDW <leotecinfo21@gmail.com>
2025-04-03 21:35:46 -0300

    Criando encapsulamento

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from personagem import Personagem
 
 
-heroi = Personagem("Meliodas", 10)
+heroi = Personagem("Meliodas", 0)
 
+
+heroi.set_nivel(2)
 heroi.exibir_status()
-
--- personagem.py
+++ personagem.py
@@ -1,9 +1,25 @@
 
 class Personagem:
     def __init__(self, nome, nivel):
-        self.nome = nome
-        self.nivel = nivel
+        self.__nome = nome
+        if nivel > 0:
+            self.nivel = nivel
+        else:
+            print("Nível precisa ser maior que 0, definindo o nível para 1")
+            self.__nivel = 1
 
     def exibir_status(self):
-        print(f"Personagem:{self.nome}")
-        print(f"Nível:{self.nivel}")
+        print(f"Personagem:{self.__nome}")
+        print(f"Nível:{self.__nivel}")
+
+    def get_nome(self):
+        return self.__nome
+    def set_nome(self,nome):
+        self.__nome = nome
+    def get_nivel(self):
+        return self.__nivel
+    def set_nivel(self,nivel):
+        if nivel > 0:
+            self.__nivel = nivel
+        else:
+            print("O nível não pode ser menor que 0")
